cfcf1a0195bc4f23d365fb5677a3248cf63af14e
Astrid Yu <astrid@astrid.tech>
2020-09-05 21:46:18 -0700

    Create benchmarks

---
+++ benchmark/bigfloat_benchmark.cpp
@@ -0,0 +1,30 @@
+#include <benchmark/benchmark.h>
+#include "../bigfloat.hpp"
+
+static void bigfloat_addition(benchmark::State& state) {
+    for (auto _ : state) {
+        bigfloat a(32.31);
+        bigfloat b(831.1329);
+        volatile auto c = a + b;
+    }
+}
+BENCHMARK(bigfloat_addition);
+
+static void double_addition(benchmark::State& state) {
+    for (auto _ : state) {
+        double a = 31.1234;
+        volatile double b = 31.1234;
+        volatile double c = a + b;
+    }
+}
+BENCHMARK(double_addition);
+
+static void bigfloat_construction(benchmark::State& state) {
+    for (auto _ : state) {
+        volatile double x = 31.1234;
+        volatile auto a = bigfloat(x);
+    }
+}
+BENCHMARK(bigfloat_construction);
+
+BENCHMARK_MAIN();
