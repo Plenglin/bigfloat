3af6205be31547c9af8f71bfa2336f721e20a8de
Astrid Yu <astrid@astrid.tech>
2020-09-18 19:16:11 -0700

    SIMD multiplication implementation

--- simd_vec4.cpp
+++ simd_vec4.cpp
@@ -10,6 +10,11 @@ using namespace bigfloat::simd;
 
 #define CONDITIONAL_INV(s, m) s ? -m : m
 
+
+simd_vec4::simd_vec4(__m256i exponent, __m256i mantissa) : exponent(exponent), mantissa(mantissa) {
+
+}
+
 simd_vec4::simd_vec4(const bf &x0, const bf &x1, const bf &x2, const bf &x3) {
     bool s0 = x0.sign();
     bool s1 = x1.sign();
@@ -17,11 +22,11 @@ simd_vec4::simd_vec4(const bf &x0, const bf &x1, const bf &x2, const bf &x3) {
     bool s3 = x3.sign();
     sign = (s3 << 3) | (s2 << 2) | (s1 << 1) | s0;
     mantissa = _mm256_set_epi64x(
-            CONDITIONAL_INV(s3, x3.mantissa),
-            CONDITIONAL_INV(s2, x2.mantissa),
-            CONDITIONAL_INV(s1, x1.mantissa),
-            CONDITIONAL_INV(s0, x0.mantissa));
-    exponent = _mm_set_epi32(x3.exponent, x2.exponent, x1.exponent, x0.exponent);
+            CONDITIONAL_INV(s3, x3.mantissa << 1),
+            CONDITIONAL_INV(s2, x2.mantissa << 1),
+            CONDITIONAL_INV(s1, x1.mantissa << 1),
+            CONDITIONAL_INV(s0, x0.mantissa << 1));
+    exponent = _mm256_set_epi64x(x3.exponent, x2.exponent, x1.exponent, x0.exponent);
 }
 
 simd_vec4::simd_vec4(bf x) : simd_vec4(x, x, x, x) {
@@ -32,16 +37,44 @@ void simd_vec4::operator+=(simd_vec4 &other) {
 
 }
 
+
 void simd_vec4::operator*=(simd_vec4 &other) {
+    // Handle signs
     sign ^= other.sign;
 
-    //
+    // Multiply mantissas
+    unsigned long muls[4];
+    for (int i = 0; i < 4; i++) {
+        unsigned __int128 mul = (unsigned __int128)mantissa_array[i] * (unsigned __int128)other.mantissa_array[i];
+        unsigned long upper = mul >> 64;
+        muls[i] = upper;
+    }
+
+    // Build vector
+    __m256i mulv = *(__m256i*)muls;
+
+    // Bootleg 64-bit sra 63 (spread sign bit across all bits) to build a mask
+    __m256i has_upper_bit_mask = _mm256_srai_epi32(mulv, 31);
+    has_upper_bit_mask = (__m256i)_mm256_permute_ps((__m256)has_upper_bit_mask, 0b11011101);
+
+    // Shift masked mantissas right by 1
+    __m256i mto_all_srl1 = _mm256_srli_epi64(mulv, 1);
+    mantissa = _mm256_blendv_epi8(mulv, mto_all_srl1, has_upper_bit_mask);
+
+    // Add exponents
+    __m256i exo = _mm256_add_epi64(exponent, other.exponent);
+
+    // Increment masked exponents by 1
+    __m256i ones = _mm256_set1_epi8(1);
+    __m256i masked_ones = _mm256_and_si256(ones, has_upper_bit_mask);
+    exponent = _mm256_add_epi64(exo, masked_ones);
 }
 
 bf simd_vec4::operator[](int i) const {
     return bf(
-            ((int*)&exponent)[i],
-            CONDITIONAL_INV((sign >> i) & 1, ((long*)&mantissa)[i]));
+            exponent_array[i],
+            CONDITIONAL_INV((sign >> i) & 1, (mantissa_array[i] >> 1))
+    );
 }
 
 void simd_vec4::invert() {
--- simd_vec4.hpp
+++ simd_vec4.hpp
@@ -8,11 +8,19 @@
 namespace bigfloat::simd {
 
     struct simd_vec4 {
-        __m256i mantissa;
-        __m128i exponent;
+        union {
+            __m256i mantissa;
+            unsigned long mantissa_array[4];
+        };
+        union {
+            // Although exponents are still 16 bits, this is more efficient for computation.
+            __m256i exponent;
+            long exponent_array[4];
+        };
         int sign;
 
         explicit simd_vec4(bf x);
+        simd_vec4(__m256i exponent, __m256i mantissa);
         simd_vec4(const bf &x0, const bf &x1, const bf &x2, const bf &x3);
 
         bf operator[](int i) const;
