07facf053af644d57da6c3136eefc86e99585ef2
Astrid Yu <astrid@astrid.tech>
2020-09-19 23:05:03 -0700

    Use MSB 62 for simd

--- simd_vec4.cpp
+++ simd_vec4.cpp
@@ -22,10 +22,10 @@ simd_vec4::simd_vec4(const bf &x0, const bf &x1, const bf &x2, const bf &x3) {
     bool s3 = x3.sign();
     sign = (s3 << 3) | (s2 << 2) | (s1 << 1) | s0;
     mantissa = _mm256_set_epi64x(
-            CONDITIONAL_INV(s3, x3.mantissa << 1),
-            CONDITIONAL_INV(s2, x2.mantissa << 1),
-            CONDITIONAL_INV(s1, x1.mantissa << 1),
-            CONDITIONAL_INV(s0, x0.mantissa << 1));
+            CONDITIONAL_INV(s3, x3.mantissa),
+            CONDITIONAL_INV(s2, x2.mantissa),
+            CONDITIONAL_INV(s1, x1.mantissa),
+            CONDITIONAL_INV(s0, x0.mantissa));
     exponent = _mm256_set_epi64x(x3.exponent, x2.exponent, x1.exponent, x0.exponent);
 }
 
@@ -34,7 +34,6 @@ simd_vec4::simd_vec4(bf x) : simd_vec4(x, x, x, x) {
 }
 
 void simd_vec4::operator+=(simd_vec4 &other) {
-
 }
 
 
@@ -45,7 +44,7 @@ void simd_vec4::operator*=(simd_vec4 &other) {
     // Multiply mantissas
     unsigned long muls[4];
     for (int i = 0; i < 4; i++) {
-        unsigned __int128 mul = (unsigned __int128)mantissa_array[i] * (unsigned __int128)other.mantissa_array[i];
+        unsigned __int128 mul = (unsigned __int128)(mantissa_array[i]<<1) * (unsigned __int128)(other.mantissa_array[i]<<1);
         unsigned long upper = mul >> 64;
         muls[i] = upper;
     }
@@ -57,14 +56,14 @@ void simd_vec4::operator*=(simd_vec4 &other) {
     __m256i zeros = _mm256_setzero_si256();
     __m256i has_upper_bit_mask = _mm256_cmpgt_epi64(zeros, mulv);
 
-    // Shift non-upper bit mantissas left by 1
-    __m256i mulv_all_sll1 = _mm256_slli_epi64(mulv, 1);
-    __m256i mto = _mm256_blendv_epi8(mulv_all_sll1, mulv, has_upper_bit_mask);
+    // Shift upper bit mantissas right by 1
+    __m256i mulv_all_srl1 = _mm256_srli_epi64(mulv, 1);
+    __m256i mto = _mm256_blendv_epi8(mulv, mulv_all_srl1, has_upper_bit_mask);
 
     // Add exponents
     __m256i exp_sum = _mm256_add_epi64(exponent, other.exponent);
 
-    // Increment upper bit exponents by 1.
+    // Decrement upper bit exponents by 1
     __m256i exo = _mm256_sub_epi64(exp_sum, has_upper_bit_mask);
 
     mantissa = mto;
@@ -74,7 +73,7 @@ void simd_vec4::operator*=(simd_vec4 &other) {
 bf simd_vec4::operator[](int i) const {
     return bf(
             exponent_array[i],
-            CONDITIONAL_INV((sign >> i) & 1, (mantissa_array[i] >> 1))
+            CONDITIONAL_INV((sign >> i) & 1, (mantissa_array[i]))
     );
 }
 
