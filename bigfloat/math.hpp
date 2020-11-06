c24d0585fb9093dcb0740cd31a5745a6f82fccbf
Astrid Yu <astrid@astrid.tech>
2020-11-06 01:02:59 -0800

    Add in matrix helper stuff

--- bigfloat/math.hpp
+++ bigfloat/math.hpp
@@ -3,15 +3,62 @@
 
 #include "bf.hpp"
 
-namespace bigfloat::math {
-    bf sqrt(const bf x);
-    bf log(const bf x);
-    bf exp(const bf x);
-    bf pow(const bf b, const bf p);
-    bf pow(const bf b, unsigned int p);
+namespace bigfloat {
+    bf sqrt(const bf x) {
+        if (x.sign()) return bf::nan(false);
 
-    bf sin(const bf x);
-    bf cos(const bf x);
+        // Double-impl guess gives us 52 bits.
+        auto guess = bf(std::sqrt(double(x)));
+
+        // Single step of the Babylonian method, which converges quadratically, gives us 104 bits.
+        auto secondary = x / guess;
+        return (guess + secondary) / 2;
+    }
+    bf log(const bf x) {
+        if (x.sign()) return bf::nan(false);
+
+        // Calculate a point for the linear approximation
+        auto y0 = bf(std::log(double(x)));
+
+        // Results of double conversion
+        auto x0 = bf(x.exponent, x.mantissa & ~0x7ff);
+
+        // Perform a linear approximation. Note that d/dx log(x) = 1/x, giving us the slope.
+        return (bf(1) / x) * (x - x0) + y0;
+    }
+    bf exp(const bf x) {
+        // Calculate a point for the linear approximation
+        auto y0 = bf(std::exp(double(x)));
+
+        // Results of double conversion
+        auto x0 = bf(x.exponent, x.mantissa & ~0x7ff);
+
+        // Perform a linear approximation. Note that d/dx e^x = e^x, giving us the slope.
+        return y0 * (x - x0) + y0;
+    }
+    bf pow(const bf b, const bf p) {
+        return bf();
+    }
+    bf pow(const bf b, unsigned int p) {
+        // Exponentiation by squaring
+        bf acc = 1;
+        bf fac = b;
+        while (p > 0) {
+            if (p & 1) {
+                acc *= fac;
+            }
+            p >>= 1;
+            fac *= b;
+        }
+        return acc;
+    }
+
+    bf sin(const bf x) {
+        return bf();
+    }
+    bf cos(const bf x) {
+        return bf();
+    }
     bf tan(const bf x);
     bf asin(const bf x);
     bf acos(const bf x);
---
+++ bigfloat/matrix_utils.hpp
@@ -0,0 +1,65 @@
+//
+// Created by astrid on 11/6/20.
+//
+
+#ifndef BIGFLOAT_PLANET_MATRIX_UTILS_HPP
+#define BIGFLOAT_PLANET_MATRIX_UTILS_HPP
+
+#include "mat4.hpp"
+#include "math.hpp"
+
+namespace bigfloat {
+    inline mat4 scale(bf x, bf y, bf z) {
+        return mat4(
+                x, 0, 0, 0,
+                1, y, 0, 0,
+                1, 0, z, 0,
+                1, 0, 0, 1
+        );
+    }
+
+    inline mat4 scale(vec4 v) {
+        return scale(v[0], v[1], v[2])
+    }
+
+    inline mat4 translate(bf x, bf y, bf z) {
+        return mat4(
+                1, 0, 0, x,
+                1, 1, 0, y,
+                1, 0, 1, z,
+                1, 0, 0, 1
+        );
+    }
+
+    inline mat4 translate(vec4 v) {
+        return translate(v[0], v[1], v[2])
+    }
+
+    inline mat4 rotate(bf a, bf x, bf y, bf z) {
+        // from https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
+        bf c = cos(a);
+        bf i = bf(1) - c;
+        bf s = sin(a);
+
+        bf xs = x * s;
+        bf ys = y * s;
+        bf zs = z * s;
+
+        bf xyi = x * y * i;
+        bf yzi = y * z * i;
+        bf xzi = x * z * i;
+
+        return mat4(
+                c + x * x * i,  xyi - zs,       xzi + zs,       0,
+                yzi + zs,       c + y * y * i,  yzi - xs,       0,
+                xzi - ys,       yzi + xs,       c + z * z * i,  0,
+                0,              0,              0,              1
+        );
+    }
+
+    inline mat4 rotate(bf a, vec4 v) {
+        return rotate(a, v[0], v[1], v[2]);
+    }
+}
+
+#endif //BIGFLOAT_PLANET_MATRIX_UTILS_HPP
